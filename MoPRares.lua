a29efd0bdaf533726f1af352c231d9fa5b5422c1
Nirgali42 <nirgali@idlenexus.net>
2013-09-30 00:58:31 -0400

    Fixed message handling
    -Messages are more efficient, reports npc less often, reports death once
    -Cleaned up code a bit

--- MoPRares.lua
+++ MoPRares.lua
@@ -1,7 +1,7 @@
 local _
 local frame	= CreateFrame("Frame", nil, UIParent)
 local textframe = CreateFrame("Frame", "MoPRaresText", UIParent)
-textframe:SetSize(200,25)
+textframe:SetSize(220,25)
 textframe:SetPoint("LEFT",200,100)
 textframe:SetMovable(true)
 textframe:EnableMouse(true)
@@ -18,52 +18,52 @@ textframe.text:SetFont("Fonts\\ARIALN.TTF",13,"OUTLINE")
 textframe.text:SetTextColor(1,0.8,0,1)
 
 local mobs = {
-	-- npc_id = {message sent, died time, name}
-	[50358] = {0, 0, 0},
-	[69664] = {0, 0, 0},
-	[69996] = {0, 0, 0},
-	[69997] = {0, 0, 0},
-	[69998] = {0, 0, 0},
-	[69999] = {0, 0, 0},
-	[70000] = {0, 0, 0},
-	[70001] = {0, 0, 0},
-	[70002] = {0, 0, 0},
-	[70003] = {0, 0, 0},
-	[73158] = {0, 0, 0},
-	[73160] = {0, 0, 0},
-	[73161] = {0, 0, 0},
-	[72909] = {0, 0, 0},
-	[72245] = {0, 0, 0},
-	[71919] = {0, 0, 0},
-	[72193] = {0, 0, 0},
-	[72045] = {0, 0, 0},
-	[71864] = {0, 0, 0},
-	[73854] = {0, 0, 0},
-	[72048] = {0, 0, 0},
-	[72769] = {0, 0, 0},
-	[73277] = {0, 0, 0},
-	[72775] = {0, 0, 0},
-	[73282] = {0, 0, 0},
-	[72808] = {0, 0, 0},
-	[73166] = {0, 0, 0},
-	[73163] = {0, 0, 0},
-	[73157] = {0, 0, 0},
-	[73170] = {0, 0, 0},
-	[73169] = {0, 0, 0},
-	[73171] = {0, 0, 0},
-	[73175] = {0, 0, 0},
-	[73173] = {0, 0, 0},
-	[73172] = {0, 0, 0},
-	[73167] = {0, 0, 0},
-	[72970] = {0, 0, 0},
-	[73279] = {0, 0, 0},
-	[73281] = {0, 0, 0},
-	[73174] = {0, 0, 0},
-	[72032] = {0, 0, 0},
-	[73666] = {0, 0, 0},
-	[73281] = {0, 0, 0},
-	[73854] = {0, 0, 0},
-	[72775] = {0, 0, 0},
+	-- npc_id = {message sent, died time, name, death reported}
+	[50358] = {0, 0, 0, 0},
+	[69664] = {0, 0, 0, 0},
+	[69996] = {0, 0, 0, 0},
+	[69997] = {0, 0, 0, 0},
+	[69998] = {0, 0, 0, 0},
+	[69999] = {0, 0, 0, 0},
+	[70000] = {0, 0, 0, 0},
+	[70001] = {0, 0, 0, 0},
+	[70002] = {0, 0, 0, 0},
+	[70003] = {0, 0, 0, 0},
+	[73158] = {0, 0, 0, 0},
+	[73160] = {0, 0, 0, 0},
+	[73161] = {0, 0, 0, 0},
+	[72909] = {0, 0, 0, 0},
+	[72245] = {0, 0, 0, 0},
+	[71919] = {0, 0, 0, 0},
+	[72193] = {0, 0, 0, 0},
+	[72045] = {0, 0, 0, 0},
+	[71864] = {0, 0, 0, 0},
+	[73854] = {0, 0, 0, 0},
+	[72048] = {0, 0, 0, 0},
+	[72769] = {0, 0, 0, 0},
+	[73277] = {0, 0, 0, 0},
+	[72775] = {0, 0, 0, 0},
+	[73282] = {0, 0, 0, 0},
+	[72808] = {0, 0, 0, 0},
+	[73166] = {0, 0, 0, 0},
+	[73163] = {0, 0, 0, 0},
+	[73157] = {0, 0, 0, 0},
+	[73170] = {0, 0, 0, 0},
+	[73169] = {0, 0, 0, 0},
+	[73171] = {0, 0, 0, 0},
+	[73175] = {0, 0, 0, 0},
+	[73173] = {0, 0, 0, 0},
+	[73172] = {0, 0, 0, 0},
+	[73167] = {0, 0, 0, 0},
+	[72970] = {0, 0, 0, 0},
+	[73279] = {0, 0, 0, 0},
+	[73281] = {0, 0, 0, 0},
+	[73174] = {0, 0, 0, 0},
+	[72032] = {0, 0, 0, 0},
+	[73666] = {0, 0, 0, 0},
+	[73281] = {0, 0, 0, 0},
+	[73854] = {0, 0, 0, 0},
+	[72775] = {0, 0, 0, 0},
 	--[69384] = {0, 0, 0}, --test crab
 }
 local message
@@ -106,14 +106,20 @@ local function init()
 end
 
 local function update(self,elapsed)
-    timer = timer + elapsed;
-    if timer >= throttle then
-		if mobs[message_mob_id][1] + 30 < GetTime() then
+	timer = timer + elapsed;
+	if timer >= throttle then
+		if mobs[message_mob_id][4] then return end -- npc death was reported
+		if mobs[message_mob_id][2] > 0 then -- npc died but not reported
+			SendChatMessage(message , "CHANNEL", nil, 1)
+			mobs[message_mob_id][4] = true
+			return
+		end
+		if mobs[message_mob_id][1] + 30 < GetTime() then -- npc spotted but not not reported
 			SendChatMessage(message , "CHANNEL", nil, 1)
 		end
-        timer = 0
+        	timer = 0
 		frame:SetScript("OnUpdate", nil)
-    end
+	end
 end
 
 local function RandomizeTime()
@@ -152,9 +158,11 @@ local function events(frame, event, ...)
 				mobs[id][1] = GetTime()
 				if string.find(msg,'%(0%%%)') then 
 					mobs[id][2] = GetTime()
+					mobs[id][4] = true
 					mobs[id][3] = string.match(msg,'[%a%s]*[^%(]',11)
-					message_mob_id = id
-					RandomizeTime()
+				else
+					mobs[id][2] = 0
+					mobs[id][4] = false
 				end
 			end
 		end
