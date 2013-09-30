6d60bbc2383aaf7b0a424a1412405e3752f8b329
Nirgali42 <nirgali@idlenexus.net>
2013-09-30 01:17:37 -0400

    Added apostrophe and hyphen support in npc names

--- MoPRares.lua
+++ MoPRares.lua
@@ -159,7 +159,7 @@ local function events(frame, event, ...)
 				if string.find(msg,'%(0%%%)') then 
 					mobs[id][2] = GetTime()
 					mobs[id][4] = true
-					mobs[id][3] = string.match(msg,'[%a%s]*[^%(]',11)
+					mobs[id][3] = string.match(msg,'[%a%s%-%']*[^%(]',11)
 				else
 					mobs[id][2] = 0
 					mobs[id][4] = false
